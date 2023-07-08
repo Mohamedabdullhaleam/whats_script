d99a77380c94f2b469493ac383038b6f3e180408
Mohamedabdullhaleam <mohamedlimo236@gmail.com>
2023-07-09 00:13:46 +0300

    update

--- whatscripttt.py
+++ whatscripttt.py
@@ -7,31 +7,39 @@ with open ('numbers.txt','r')as r:
 # Define a list of WhatsApp numbers
     x=w.split('\n')
     print(x)
-
 # Get the current time hours , mins
-hours = datetime.now().hour
-minutes = datetime.now().minute
-
-
-
+count = 0
 numbers = x
-
 # Iterate through the list and send messages
 for num in numbers:
-   # message to be sent
-    message ="""i whish this message find you well 
-                my regards """
+    hours = datetime.now().hour
+    minutes = datetime.now().minute
     # critical for the script to keep it running
-    min = minutes+1
+    if (minutes < 59) :
+        min = minutes + 1
+    else :
+        hours = hours +1
+        min =1
+    print(hours,min)
+   # message to be sent
+
+    message ="""
+     type your text here
+     .....................
+     ................... 
+      
+"""
 
     # Sending the message to the current number
-    kit.sendwhatmsg(f'+2'+num ,message,hours,min) # 10 represents hour, 30 represents minute
-
-    # Delay between sending messages (to avoid spamming)
-    # You can adjust this delay based on your needs
+    kit.sendwhatmsg(num ,message,hours,min,20,20) #
+    
     # Example: time.sleep(10)
 
     print(f"Message sent to {num}")
-    time.sleep(10)
-
+    # Delay between sending messages (to avoid spamming)
+    # You can adjust this delay based on your needs
+    time.sleep(10)    
+    count+=1
+    print(count)
 print("All messages sent successfully!")
+
